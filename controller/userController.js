2639e1c5643019b9270465ef66dda1234c048964
ONKAR <borgaonkar1998@gmail.com>
2023-05-24 09:55:27 +0530

    updated auth

--- controller/userController.js
+++ controller/userController.js
@@ -1,37 +1,70 @@
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
-exports.register = async(req,res)=>{
+exports.register = async (req, res) => {
     try {
-        
-        const {password} = req.body
-        const hasPass = await bcrypt.hash(password,10)
 
- const result =  await User.create({...req.body, password:hasPass}) // add data from body and haspass
-   res.json({message:"user register success", result})
+        const { password, email } = req.body
+        const found = await User.findOne({ email })   // to check email is register or not 
+        if (found) {
+            return res.json({
+                message: "email already exist"
+            })
+        }
+
+
+        const hasPass = await bcrypt.hash(password, 10)
+
+        const result = await User.create({ ...req.body, password: hasPass }) // add data from body and haspass
+        res.json({ message: "user register success", result })
 
     } catch (error) {
-        res.json({message:"somethingwent wrong", error})
+        res.json({ message: "somethingwent wrong" + error })
     }
 }
-exports.fetchUsers = async(req,res)=>{
+exports.fetchUsers = async (req, res) => {
     try {
-        
- const result =  await User.find()
-   res.json({message:"user fetch success", result})
+        const token = req.headers.authorization
+        if (!token) {
+           return res.json({message:"provide token"})
+        }
+        jwt.verify(token,process.env.JWT_KEY)
+        const result = await User.find()
+        res.json({ message: "user fetch success", result })
 
     } catch (error) {
-        res.json({message:"somethingwent wrong", error})
+        res.json({ message: "somethingwent wrong" + error })
+    }
+}
+exports.login = async (req, res) => {
+    try {
+
+        const { email, password } = req.body
+        const result = await User.findOne({ email })
+
+        if (!result) {
+            return res.json({ message: "email is not register with us" })
+        }
+        const match = await bcrypt.compare(password, result.password)
+        if (!match) {
+            return res.json({ message: " incorrect password" })
+        } 
+        const token = jwt.sign({ name: "kate" }, process.env.JWT_KEY)
+        res.json({ message: "login success", token })
+
+
+    } catch (error) {
+        res.json({ message: "somethingwent wrong login " + error })
     }
 }
 
-// exports.updateUser = async(req,res)=>{
-//     try {
-        
-//  const result =  await User.()
-//    res.json({message:"user fetch success", result})
+exports.destory = async (req, res) => {
+    try {
+        await User.deleteMany()
+        res.json({ message: "user destroyed successfully" })
+    } catch (error) {
+        res.json({ message: "somethingwent wrong" + error })
+    }
+}
 
-//     } catch (error) {
-//         res.json({message:"somethingwent wrong", error})
-//     }
-// }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
-const { fetchUsers, register } = require("../controller/userController")
+const { fetchUsers, register, login, destory } = require("../controller/userController")
 
 const router  = require("express").Router()
 
 router
 .get("/",fetchUsers)
 .post("/register",register)
+.post("/login",login)
+.delete("/destroy",destory)
 
 
 module.exports = router
